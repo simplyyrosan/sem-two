38501e39631a2d7b39c9d160373db5c1c27db234
simplyyrosan <ommprakash825@gmail.com>
2026-03-18 10:28:42 +0530

    q4

--- src/lab2/Q04.java
+++ src/lab2/Q04.java
@@ -31,13 +31,27 @@ class Deposit {
     }
 
     void display() {
-        System.out.println();
+        System.out.println("Pricipal: " + principal + "\nTime: " + time + "\nRate: " + rate + "\nTotal amt: " + total_amt + "----------");
+    }
+
+    void cacl_amt() {
+        total_amt = principal + (principal*rate*time)/100;
     }
 
 }
 
 public class Q04 {
 
-
+    public static void main(String[] args) {
+        Deposit dp1 = new Deposit();
+        Deposit dp2 = new Deposit(100000, 4, 5);
+        Deposit dp3 = new Deposit(10000, 5.6);
+        dp1.cacl_amt();
+        dp2.cacl_amt();
+        dp3.cacl_amt();
+        dp1.display();
+        dp2.display();
+        dp3.display();
+    }
     
 }
