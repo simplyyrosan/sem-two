db1cdd5945c4b1b7a864aa7ec9ca72289fd11515
simplyyrosan <ommprakash825@gmail.com>
2026-04-18 02:03:59 +0530

    .

--- src/lab3/Q01.java
+++ src/lab3/Q01.java
@@ -1,16 +1,10 @@
 package lab3;
 import java.util.*;
 
-class NumberFormatException extends Exception {
-	NumberFormatException(String msg) {
-		super(msg);
-	}
-}
-
 public class Q01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your lucky number: ");
 		int n = sc.nextInt();
@@ -24,8 +18,7 @@ public class Q01 {
 			System.out.println(e.getMessage());
 		}
 		
-		
-		
+	
 	}
 
 }
--- src/lab3/Q02.java
+++ src/lab3/Q02.java
@@ -1,10 +1,28 @@
 package lab3;
 
 public class Q02 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String[] colors = null;  // not initialized (can cause NullPointerException)
 
-	}
+        try {
+            // initializing array
+            colors = new String[3];
+            colors[0] = "Blue";
+            colors[1] = "Black";
+            colors[2] = "Green";
 
+            // accessing elements
+            for (int i = 0; i <= 3; i++) {  // intentional mistake (index 3 is invalid)
+                System.out.println(colors[i]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Array index is out of bounds.");
+        } catch (NullPointerException e) {
+            System.out.println("Error: Array is not initialized.");
+        } finally {
+            System.out.println("Program execution completed.");
+        }
+    }
 }
